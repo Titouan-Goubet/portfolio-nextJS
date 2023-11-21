401c6eba6340cc40fa47e259206227e3cf3ebf98
Titouan Goubet <titouangoubet@hotmail.fr>
2023-11-21 14:06:12 +0100

    Petit fix de version de dépendances

--- components/Submit-btn.tsx
+++ components/Submit-btn.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useFormStatus } from 'react-dom'
+import { useFormStatus } from "react-dom"
 import { FaPaperPlane } from 'react-icons/fa'
 
 export default function SubmitBtn() {
